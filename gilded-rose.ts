815866c88471fe911df5c7054935596b1b2b5482
nathan <nathan_santos@usp.br>
2021-10-04 13:00:19 -0300

    refactor: alteracao de ordem de codigo

--- gilded-rose.ts
+++ gilded-rose.ts
@@ -23,13 +23,11 @@ export class GildedRose {
                 return;
             }
             if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
-                }
                 if (item.sellIn < 0) {
                     item.quality = 0;
+                }
+                else if (item.sellIn < 11) {
+                    item.quality = item.quality + 1
                 }
                 return;
             }
@@ -40,10 +38,10 @@ export class GildedRose {
                 }
                 return;
             }
+            item.sellIn = item.sellIn - 1;
             if (item.quality > 0) {
                 item.quality = item.quality - 1
             }
-            item.sellIn = item.sellIn - 1;
             if (item.sellIn < 0) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1
